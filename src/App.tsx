8702df6c1b15ea83b4a35e49bfb5defb17e9a79f
joker69216 <hamaraad883@gmail.com>
2023-01-16 10:43:37 +0300

    add todo model to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import Todos from './components/Todos';
+import TodoClass from './Models/Todo'
 
 function App() {
+  const todos = [
+    new TodoClass('Learn React'),
+    new TodoClass('Learn TypeScript')
+  ]
   return (
     <div className="App">
       <Todos items={['Learn React', 'Learn TypeScript']} />
